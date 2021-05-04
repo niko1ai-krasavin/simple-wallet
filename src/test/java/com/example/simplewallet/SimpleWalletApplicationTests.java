c63dd20941e06e787ec834b2b3b202a509949f93
niko1ai-krasavin <kosn91@mail.ru>
2021-05-04 11:01:40 +0200

    Init commit

---
+++ src/test/java/com/example/simplewallet/SimpleWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
