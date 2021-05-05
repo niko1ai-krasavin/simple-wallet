804d97b57aed0209dfd630853b52f281bc21e24e
niko1ai-krasavin <kosn91@mail.ru>
2021-05-05 19:25:10 +0200

    Correcting business logic #2 and bug fixing #2

--- src/main/java/com/example/simplewallet/validators/WalletApiValidator.java
+++ src/main/java/com/example/simplewallet/validators/WalletApiValidator.java
@@ -18,28 +18,30 @@ public class WalletApiValidator {
 
     public synchronized boolean doMoneyTransferValidation(TransferMoneyDTO transferMoneyDTO, Long senderId) {
 
-        Wallet walletSender = SimpleWalletApplication.WALLETS.get(senderId);
-        Wallet walletReceiver = SimpleWalletApplication.WALLETS.get(transferMoneyDTO.getReceiverId());
-        BigDecimal balanceSender = walletSender.getBalance();
-        BigDecimal amount = new BigDecimal(transferMoneyDTO.getAmount()).setScale(2, RoundingMode.HALF_UP);
+        synchronized (SimpleWalletApplication.WALLETS) {
+            Wallet walletSender = SimpleWalletApplication.WALLETS.get(senderId);
+            Wallet walletReceiver = SimpleWalletApplication.WALLETS.get(transferMoneyDTO.getReceiverId());
+            BigDecimal balanceSender = walletSender.getBalance();
+            BigDecimal amount = new BigDecimal(transferMoneyDTO.getAmount()).setScale(2, RoundingMode.HALF_UP);
 
-        if(walletReceiver == null) {
-            throw new WalletNotFoundException(transferMoneyDTO.getReceiverId());
+            if(walletReceiver == null) {
+                throw new WalletNotFoundException(transferMoneyDTO.getReceiverId());
+            }
+
+            if (BigDecimal.ZERO.compareTo(amount) >= 0) {
+                throw new AmountLessThanOrEqualZeroException();
+            }
+
+            if (isSenderAndReceiverTheSameWallets(senderId, transferMoneyDTO.getReceiverId())) {
+                throw new SenderAndReceiverTheSameException();
+            }
+
+            if (isNotEnoughMoneyOnWalletSender(balanceSender, amount)) {
+                throw new LowBalanceWalletException(balanceSender, amount);
+            }
+
+            return true;
         }
-
-        if (BigDecimal.ZERO.compareTo(amount) >= 0) {
-            throw new AmountLessThanOrEqualZeroException();
-        }
-
-        if (isSenderAndReceiverTheSameWallets(senderId, transferMoneyDTO.getReceiverId())) {
-            throw new SenderAndReceiverTheSameException();
-        }
-
-        if (isNotEnoughMoneyOnWalletSender(balanceSender, amount)) {
-            throw new LowBalanceWalletException(balanceSender, amount);
-        }
-
-        return true;
     }
 
     public synchronized boolean doWalletValidation(WalletDTO walletDTO) {
@@ -50,10 +52,12 @@ public class WalletApiValidator {
     }
 
     public synchronized boolean isWalletExist(long id) {
-        if (SimpleWalletApplication.WALLETS.get(id) == null) {
-            throw new WalletNotFoundException(id);
+        synchronized (SimpleWalletApplication.WALLETS) {
+            if (SimpleWalletApplication.WALLETS.get(id) == null) {
+                throw new WalletNotFoundException(id);
+            }
+            return true;
         }
-        return true;
     }
 
     private boolean isNotEnoughMoneyOnWalletSender(BigDecimal balanceSender, BigDecimal amount) {
